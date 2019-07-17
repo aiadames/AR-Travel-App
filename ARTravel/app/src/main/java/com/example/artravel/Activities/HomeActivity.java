d128a84ad769cd30e64e09eee0a3420292e2e97c
ebaena22 <51726248+ebaena22@users.noreply.github.com>
2019-07-16 17:15:17 -0700

    Add files via upload

---
+++ ARTravel/app/src/main/java/com/example/artravel/Activities/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.artravel.Activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.artravel.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
