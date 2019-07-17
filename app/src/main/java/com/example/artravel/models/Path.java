1a719c7af5c263acdc392335bb459bb54bfb478e
Ariana Adames <ariana.adames2@gmail.com>
2019-07-17 16:01:27 -0700

    Set up Recycler View with Path Card Views

--- app/src/main/java/com/example/artravel/models/Path.java
+++ app/src/main/java/com/example/artravel/models/Path.java
@@ -4,11 +4,34 @@ import com.parse.ParseClassName;
 import com.parse.ParseObject;
 
 @ParseClassName("Path")
-public class Path extends ParseObject {
+public class Path  {
     private static final String KEY_STOP_1 = "stop1";
     private static final String KEY_STOP_2 = "stop2";
     private static final String KEY_STOP_3 = "stop3";
     private static final String KEY_STOP_4 = "stop4";
     private static final String KEY_STOP_5 = "stop5";
+
+    private String mImageUrl;
+    private String mPathTitle;
+    private String mPathDescription;
+
+    public Path(String imageUrl, String pathTitle, String pathDescription){
+        mImageUrl = imageUrl;
+        mPathTitle = pathTitle;
+        mPathDescription = pathDescription;
+    }
+
+    public String getImageUrl(){
+        return mImageUrl;
+    }
+
+    public String getPathTitle(){
+        return mPathTitle;
+    }
+
+    public String getPathDescription(){
+        return mPathDescription;
+    }
+
 
 }
