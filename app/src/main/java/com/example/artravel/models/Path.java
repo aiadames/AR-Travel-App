93c08e46a68db5ce02d92a5394cd80a960bf8573
Elizabeth Baena <ebaena@fb.com>
2019-07-17 17:19:09 -0700

    Merge branches 'elizabethBranch' and 'master' of https://github.com/aiadames/AR-Travel-App

--- app/src/main/java/com/example/artravel/models/Path.java
+++ app/src/main/java/com/example/artravel/models/Path.java
@@ -4,7 +4,7 @@ import com.parse.ParseClassName;
 import com.parse.ParseObject;
 
 @ParseClassName("Path")
-public class Path  {
+public class Path {
     private static final String KEY_STOP_1 = "stop1";
     private static final String KEY_STOP_2 = "stop2";
     private static final String KEY_STOP_3 = "stop3";
@@ -15,30 +15,30 @@ public class Path  {
     public Path() {
 
     }
-
-    public Stop getStop1() {
-        return (Stop)getParseObject(KEY_STOP_1);
-    }
-
-    public Stop getStop2() {
-        return (Stop)getParseObject(KEY_STOP_2);
-    }
-
-    public Stop getStop3() {
-        return (Stop)getParseObject(KEY_STOP_3);
-    }
-
-    public Stop getStop4() {
-        return (Stop)getParseObject(KEY_STOP_4);
-    }
-
-    public Stop getStop5() {
-        return (Stop)getParseObject(KEY_STOP_5);
-    }
-
-    public String getPathName() {
-        return getString(KEY_PATH_NAME);
-    }
+//
+//    public Stop getStop1() {
+//        return (Stop)getParseObject(KEY_STOP_1);
+//    }
+//
+//    public Stop getStop2() {
+//        return (Stop)getParseObject(KEY_STOP_2);
+//    }
+//
+//    public Stop getStop3() {
+//        return (Stop)getParseObject(KEY_STOP_3);
+//    }
+//
+//    public Stop getStop4() {
+//        return (Stop)getParseObject(KEY_STOP_4);
+//    }
+//
+//    public Stop getStop5() {
+//        return (Stop)getParseObject(KEY_STOP_5);
+//    }
+//
+//    public String getPathName() {
+//        return getString(KEY_PATH_NAME);
+//    }
 
     private String mImageUrl;
     private String mPathTitle;
