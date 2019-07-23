96ddf3c5fd4818f18ebdcdb3bcf888433359b7ca
Ariana Adames <ariana.adames2@gmail.com>
2019-07-22 22:16:09 -0700

    Added new persistence override method for HomeActivity

--- app/src/main/java/com/example/artravel/Activities/HomeActivity.java
+++ app/src/main/java/com/example/artravel/Activities/HomeActivity.java
@@ -72,5 +72,18 @@ public class HomeActivity extends AppCompatActivity {
             }
             // bottomNavigationView.setSelectedItemId(R.id.action_home);
         });
+
+
+
     }
+
+    @Override
+    public void onBackPressed() {
+        if (getSupportFragmentManager().getBackStackEntryCount() > 0) {
+            getSupportFragmentManager().popBackStack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
 }
